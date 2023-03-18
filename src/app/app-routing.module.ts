1e4843d4dbb4b4ba81c7b6a2092cf17d35f1cf36
Sage Belknap <sage.belknap@mykenzie.snhu.edu>
2023-03-17 18:45:01 -0600

    Added a simple top bar that will stay at the top of the screen for each page so far. it's not perfect but routing is set up for the home ( click the logo) and the login page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeViewComponent} from "./components/home-view/home-view.component";
-import {LoginViewComponent} from "./components/login-view/login-view.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeViewComponent } from "./components/home-view/home-view.component";
+import { LoginViewComponent } from "./components/login-view/login-view.component";
 
 const routes: Routes = [
-  {path: '', component: HomeViewComponent},
-  {path: 'login', component: LoginViewComponent}
+  { path: '', component: HomeViewComponent },
+  { path: 'login', component: LoginViewComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
